97339675e767f5e23c63ea4b56577f9623287c1f
papaw5254 <papaw5254@gmail.com>
2026-07-09 08:34:35 +0300

    Revert "Add Next API rewrite"
    This reverts commit 7d47583884f82fea073edd89089777cc3584a7e6.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -11,14 +11,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['localhost'],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://185.191.141.138:4000/api/:path*',
-      },
-    ]
-  },
   experimental: {
     workerThreads: false,
     cpus: 1,
