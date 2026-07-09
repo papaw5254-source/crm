7d47583884f82fea073edd89089777cc3584a7e6
papaw5254 <papaw5254@gmail.com>
2026-07-09 08:24:56 +0300

    Add Next API rewrite

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -11,6 +11,14 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['localhost'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://185.191.141.138:4000/api/:path*',
+      },
+    ]
+  },
   experimental: {
     workerThreads: false,
     cpus: 1,
